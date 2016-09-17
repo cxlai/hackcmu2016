cf472bbf847b94b1021fbd6c293cb23eec56a9c2
Becky Jesurum <bljesurum@gmail.com>
2016-09-17 09:44:20 +0100

    added documentation

--- algorithm.js
+++ algorithm.js
@@ -1,104 +1,78 @@
-/*
-user input:
-	dist : desired path distance
-	start : starting location
-
-const numwaypoints = 23?
-const d = dist/pi
-use geocoord API to convert start to coordinates if not already -> (x,y)
-angle = random()*2pi
-//diampoint = (x+dcos(angle),y+dsin(angle))
-midpoint = (x+dcos(angle)/2,y+dsin(angle)/2) = (mx,my)
-
-create an array angles, filled with numwaypoints random angles [random()*2pi]
-calculate angle from 0 to start w.r.t midpoint -> a
-sort array with comparison function
-fun comp (a1,a2) = 
-	a1 -= angle
-	a2 -= angle
-	if a1 < 0 then a1 += 2pi
-	if a2 < 0 then a2 += 2pi
-	return a-b
+/*//http://www.movable-type.co.uk/scripts/latlong.html
+function getDist (lat1,lat2) {
+	var R = 3959; // radius of earth in miles
+	var φ1 = lat1.toRadians();
+	var φ2 = lat2.toRadians();
+	var Δφ = (lat2-lat1).toRadians();
+	var Δλ = (lon2-lon1).toRadians();
 	
-create new array coords s.t. coords[i] = (mx+d/2cos(angles[i]),my+d/2sin(angles[i]))
-*/
-//http://www.movable-type.co.uk/scripts/latlong.html
-/*function getDist (lat1,lat2) {
-		var R = 3959; // radius of earth in miles
-    var φ1 = lat1.toRadians();
-    var φ2 = lat2.toRadians();
-    var Δφ = (lat2-lat1).toRadians();
-    var Δλ = (lon2-lon1).toRadians();
-		
-		var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
-						Math.cos(φ1) * Math.cos(φ2) *
-						Math.sin(Δλ/2) * Math.sin(Δλ/2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-
-    return R * c;
+	var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+					Math.cos(φ1) * Math.cos(φ2) *
+					Math.sin(Δλ/2) * Math.sin(Δλ/2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+	return R * c;
 }*/
 
-// for lat, make φ1, φ2 = 0
-// for long, get φ2 from lat calculation
+// inverse of the above function, taken from the internet
+// d is an x OR y component. If x, p1 and p2 are 0.
 function getLatLong (d, p1, p2) {
-    var sign = d / Math.abs(d);
-    d = Math.abs(d);
-	var R = 3959; // radius of earth in miles
+	var sign = d / Math.abs(d);
+	d = Math.abs(d);
+	var R = 3959; // radius of earth in miles, source google
 	var c = d / R;
-	var a = Math.tan(c/2);
-    // console.log("d", d, "p1", p1, "p2", p2, "c", c, "a", a, "asin", Math.asin(Math.sqrt(a)));
-	return sign * 2 * Math.asin(Math.sqrt(a) / (Math.cos(p1)*Math.cos(p2)));
+	var a = Math.tan(c / 2);
+	return sign * 2 * Math.asin(Math.sqrt(a) / (Math.cos(p1) * Math.cos(p2)));
 }
 
+// creates an array of random angles from 0 to 2*PI
 function makeArray (n) {
 	var arr = [];
-	for (var i=0; i<n; i++) {
-      arr.push((Math.random() + i) * 2.0 / 5.0 * Math.PI);
+	for (var i = 0; i < n; i++) {
+		arr.push((Math.random() + i) * 2.0 / n * Math.PI);
 	}
 	return arr;
 }
 
+// creates a loop of length dist from start
 function getPath (dist, start) {
 	var numWayPoints = 5;
 	var d = dist/Math.PI;
 	
-	var getRandomAngle = function () {return Math.random() * 2 * Math.PI};
-	
-	// assume start is in geo-coords
 	var x = start.lat();
 	var y = start.lng();
-	var angle = getRandomAngle();
-	var dx = getLatLong(d*Math.cos(angle),0,0);
-	var dy = getLatLong(d*Math.sin(angle),x,dx);
-    // midpoint/pivot coords
-	var px = x + dx/2;
-	var py = y + dy/2;
+	// pick a random direction around which the loop will be centered
+	var angle = Math.random() * 2 * Math.PI;
+	// convert distance offsets to latitude/longitude locations
+	var dx = getLatLong(d * Math.cos(angle), 0, 0);
+	var dy = getLatLong(d * Math.sin(angle), x, x + dx);
+  // midpoint/pivot
+	var px = x + dx / 2.0;
+	var py = y + dy / 2.0;
+	
+	// get waypoints to make the loop, all the same dist from pivot
 	var angles = makeArray(numWayPoints);
+	// map distance to longitudes/latitudes given an angle
 	var mapfun = function (ang) {
-		var dpx = getLatLong(d/2*Math.cos(ang),0,0);
-		var dpy = getLatLong(d/2*Math.sin(ang),px,px+dpx);
-		//return new google.maps.DirectionsWaypoint(new google.maps.LatLng(dpx,dpy));
-        return {location: new google.maps.LatLng(px+dpx,py+dpy), stopover: false};
+		var dpx = getLatLong(d / 2.0 * Math.cos(ang), 0, 0);
+		var dpy = getLatLong(d / 2.0 * Math.sin(ang), px, px + dpx);
+    return {location: new google.maps.LatLng(px + dpx, py + dpy), 
+						stopover: false};
 	}
 	var waypts = angles.map(mapfun);
-    var request = {
-      origin: start,
-      travelMode: 'WALKING',
-      waypoints: waypts,
-      destination: start,
-      provideRouteAlternatives: false,
-      optimizeWaypoints: true
-    };
-    console.log(start.lat(), start.lng());
-    //console.log(waypts[0].lat(), waypts[0].lng());
-    //console.log(typeof(waypts[0]));
-    console.log(waypts.map(function (x) {return [x.location.lat(), x.location.lng()]}));
-    directionsService.route(request, function(result, status) {
-      if (status == 'OK') {
-        directionsDisplay.setDirections(result);
-      } else {
-        console.log(status);
-      }
-    });
+	var request = {
+		origin: start,
+		travelMode: 'WALKING',
+		waypoints: waypts,
+		destination: start,
+		provideRouteAlternatives: false,
+		optimizeWaypoints: true
+	};
+	
+	directionsService.route(request, function(result, status) {
+		if (status == 'OK') {
+			directionsDisplay.setDirections(result);
+		} else {
+			console.log(status);
+		}
+	});
 }
-
